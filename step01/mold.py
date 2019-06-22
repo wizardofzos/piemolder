7fc244b75313530e1af3cc677427dfc8263b9d2f
Wizard of z/OS <wizard@zdevops.com>
2019-06-22 09:28:58 +0200

    link to origin

--- step01/mold.py
+++ step01/mold.py
@@ -1,6 +1,7 @@
 # Initial version of mol
 # It just adds the comment "This pie has molded" at the start of 
 # all .py files we fidn in the current folder...
+# Based on:https://www.youtube.com/watch?v=2Ra1CCG8Guo
 
 import glob
 import os
